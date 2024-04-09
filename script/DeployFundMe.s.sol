8e11d959d54453a766b576588dd2aa7bcf70e8cd
Antonio Gabriel <AntonioGabriel@MacBook-Pro-de-David-2.local>
2024-04-09 13:38:32 -0300

    chore: forge init

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.25;
 
 import {script} from "forge-std/Script.sol";
 import  {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract deploy FundMe is Script{
     function run() external return (fundMe){
+        HelperConfig helperConfig = new helperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        
         vm.StartBroadcast();
         // Mock
-        new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
